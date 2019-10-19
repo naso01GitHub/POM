a8410eec0d2c0bea98563bce560cb6ce206b05e1
nassovv <nassovv@yahoo.com>
2019-10-19 23:02:31 +0300

    add

---
+++ Homework_7_Rework/Pages_Solution _Grid/Pages/LogIn_Page/LogIn_Page.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace Pages_Solution.Pages
+{
+    public class LogIn_Page : BasePage
+    { 
+        string sMail = "1_1_1_3@123.com";
+        public LogIn_Page(IWebDriver driver)
+            : base(driver)
+        {
+        }
+
+
+
+        public new string Url => "http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation";
+        public IWebElement Email => Driver.FindElement(By.Id("email_create"));
+        public IWebElement Submit => Driver.FindElement(By.Id("SubmitCreate"));   
+        
+        public void SendDefaultMail()
+        {
+            Email.SendKeys(sMail);
+        }
+     
+        //public new void Navigate()
+        //{
+        //   Driver.Url = "http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation";
+       //}
+    }
+}
---
+++ Homework_7_Rework/Pages_Solution _Grid/Pages/Registration_Page/Registration_Page_Asserts.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Pages_Solution.Pages
+{
+    public partial class Registration_Page : BasePage
+    {
+       
+
+        public void Check_Error_Messages_Count()
+        {
+            string BaseErrorMessage = Get_Web_BaseError_Message();
+            if (ExpectedErrorMessages.Count == 1)
+            {
+                Assert.IsTrue(BaseErrorMessage.Contains("There is " + ExpectedErrorMessages.Count.ToString()), "The error count in Main Error message is wrong.");
+            }
+            else if (ExpectedErrorMessages.Count > 1)
+            {
+                Assert.IsTrue(BaseErrorMessage.Contains("There are " + ExpectedErrorMessages.Count.ToString()), "The error count in Main Error message is wrong.");
+            }
+        }
+
+
+
+
+        public void Check_Error_Messages_Pressent()
+        {
+            List<string> Errors = Get_Web_Error_Messages();
+            for (int i = 0; i < ExpectedErrorMessages.Count; i++)
+            {
+                Assert.IsTrue(Errors.Contains(ExpectedErrorMessages[i]), "Error " + ExpectedErrorMessages[i] + "does not exist.");
+            }
+        }
+
+
+
+        public void Navigate(LogIn_Page page)
+        {
+            page.Navigate("http://automationpractice.com/index.php?controller=authentication&back=my-account#account-creation");
+            var Before_Attr_Color = page.Email.GetCssValue("color");
+            page.SendDefaultMail();
+            page.Submit.Click();
+            Assert.IsTrue(ExtendedMethods.Wait_for_Color(page.Email, "rgba(53, 179, 63, 1)", 10), "Email is not accepted");
+        }
+    }
+}
